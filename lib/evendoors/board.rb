2dad5aaa3275552f3c7767707b197475d9f8abfd
Jérémy Zurcher <jeremy@asynk.ch>
2012-05-08 23:25:49 +0200

    Board, inherits from Door, only override process_p

--- lib/evendoors/board.rb
+++ lib/evendoors/board.rb
@@ -4,24 +4,11 @@
 #
 module EvenDoors
     #
-    class Board < Spot
+    class Board < Door
         #
         def initialize n, p=nil
             super n, p
-            @saved = nil
             @postponed = {}
-            @parent.add_spot self if @parent
-        end
-        #
-        def require_p p_kls
-            p = EvenDoors::Twirl.require_p p_kls
-            p.src = self
-            p
-        end
-        #
-        def release_p p
-            @saved=nil if @saved==p     # particle is released, all is good
-            EvenDoors::Twirl.release_p p
         end
         #
         def process_p p
@@ -41,23 +28,6 @@ module EvenDoors
                 release_p @saved
                 @saved = nil
             end
-        end
-        #
-        def process_sys_p p
-            # nothing todo with it now
-            EvenDoors::Twirl.release_p p
-        end
-        #
-        def send_p p
-            p.src = self
-            @saved=nil if @saved==p # particle is sent back the data, all is good
-            @parent.send_p p        # daddy will know what to do
-        end
-        #
-        def send_sys_p p
-            p.src = self
-            @saved=nil if @saved==p # particle is sent back the data, all is good
-            @parent.send_sys_p p    # daddy will know what to do
         end
         #
     end
