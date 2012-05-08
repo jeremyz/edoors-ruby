0fccc3b247a7d4e0364cbebc80e89e8797c30e29
Jérémy Zurcher <jeremy@asynk.ch>
2012-05-08 17:00:59 +0200

    specs: complete Link coverage

--- spec/evendoors_spec.rb
+++ spec/evendoors_spec.rb
@@ -212,4 +212,23 @@ describe EvenDoors do
         #
     end
     #
+    describe EvenDoors::Link do
+        #
+        it "from particle data" do
+            p = EvenDoors::Twirl.require_p EvenDoors::Particle
+            p.set_data EvenDoors::LNK_SRC, 'input1'
+            p.set_data EvenDoors::LNK_DSTS, 'concat1?follow,output1'
+            p.set_data EvenDoors::LNK_FIELDS, 'f0,f2'
+            p.set_data EvenDoors::LNK_CONDF, 'f0,f1,f2'
+            p.set_data EvenDoors::LNK_CONDV, 'v0v1v2'
+            lnk = EvenDoors::Link.from_particle_data p
+            lnk.src.should eql 'input1'
+            lnk.dsts.should eql 'concat1?follow,output1'
+            lnk.fields.should eql 'f0,f2'
+            lnk.cond_fields.should eql 'f0,f1,f2'
+            lnk.cond_value.should eql 'v0v1v2'
+        end
+        #
+    end
+    #
 end
