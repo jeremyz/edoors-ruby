3a6b898f8e8d103b3858948e2bc1a187d97cbcc5
Jérémy Zurcher <jeremy@asynk.ch>
2012-05-10 22:28:18 +0200

    specs: add link->json->link

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -25,6 +25,16 @@ describe EvenDoors::Link do
         lnk.cond_value.should eql 'v0v1v2'
     end
     #
+    it "link->json->link" do
+        link = EvenDoors::Link.new  'input1', 'concat1?follow,output1', 'f0,f2', 'f0,f1,f2', 'v0v1v2'
+        lnk = EvenDoors::Link.json_create( JSON.load( JSON.generate(link) ) )
+        link.src.should eql lnk.src
+        link.dsts.should eql lnk.dsts
+        link.fields.should eql lnk.fields
+        link.cond_fields.should eql lnk.cond_fields
+        link.cond_value.should eql lnk.cond_value
+    end
+    #
 end
 #
 # EOF
