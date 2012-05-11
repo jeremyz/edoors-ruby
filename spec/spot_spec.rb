831fbca253ad01a4c9f38f18fed2afba4dc8226f
Jérémy Zurcher <jeremy@asynk.ch>
2012-05-11 11:26:09 +0200

    specs: adapt to new Spot behavior

--- spec/spot_spec.rb
+++ spec/spot_spec.rb
@@ -6,15 +6,15 @@ require 'spec_helper'
 #
 describe EvenDoors::Spot do
     #
-    before(:each) do
-        EvenDoors::Spin.clear!
-    end
-    #
     it "path construction" do
-        s0 = EvenDoors::Spot.new 'top', nil
-        s1 = EvenDoors::Spot.new 'room0', s0
-        s2 = EvenDoors::Spot.new 'room1', s1
-        s3 = EvenDoors::Spot.new 'door', s2
+        class S<EvenDoors::Spot
+            def add_spot s
+            end
+        end
+        s0 = S.new 'top', nil
+        s1 = S.new 'room0', s0
+        s2 = S.new 'room1', s1
+        s3 = S.new 'door', s2
         s3.path.should eql 'top/room0/room1/door'
         lambda { EvenDoors::Spot.new('do/or0', nil) }.should raise_error(EvenDoors::Exception)
         lambda { EvenDoors::Spot.new('/door0', nil) }.should raise_error(EvenDoors::Exception)
