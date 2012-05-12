151c9604a25be7c2f1e2f2818f927e17bcd3f22f
Jérémy Zurcher <jeremy@asynk.ch>
2012-05-13 00:11:32 +0200

    Particle: require time

--- lib/evendoors/particle.rb
+++ lib/evendoors/particle.rb
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with evendoors-ruby.  If not, see <http://www.gnu.org/licenses/>.
 
+require 'time'
 #
 module EvenDoors
     #
