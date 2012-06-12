0721a8d0c68d51057898d7b8a467be556913b797
Jérémy Zurcher <jeremy@asynk.ch>
2012-06-13 00:24:36 +0200

    Door: _garbage directly calls @spin.release_p

--- lib/edoors/door.rb
+++ lib/edoors/door.rb
@@ -54,7 +54,7 @@ module Edoors
         def _garbage
             puts " ! #{path} didn't give back #{@saved}" if @spin.debug_errors
             puts "\t#{@saved.data Edoors::FIELD_ERROR_MSG}" if @saved.action==Edoors::ACT_ERROR
-            release_p @saved
+            @spin.release_p @saved
             @saved = nil
         end
         private :_garbage
